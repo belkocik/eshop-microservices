5ba0e0ce4bc3c25a1f56f20b9b0c4c757aaa3fe3
Marcin Niedbalec <marcin.niedbalec@proton.me>
2025-06-24 22:30:34 +0200

    Ordering.Infrastructure

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -2,11 +2,35 @@ namespace Ordering.Domain.ValueObjects;
 
 public record Address
 {
+  protected Address()
+  {
+  }
+
+  private Address(string firstName, string lastName, string emailAddress, string addressLine, string country,
+    string state, string zipCode)
+  {
+    FirstName = firstName;
+    LastName = lastName;
+    EmailAddress = emailAddress;
+    AddressLine = addressLine;
+    Country = country;
+    State = state;
+    ZipCode = zipCode;
+  }
+
   public string FirstName { get; } = string.Empty;
   public string LastName { get; } = string.Empty;
-  public string? EmailAddress { get; } = null;
+  public string? EmailAddress { get; }
   public string AddressLine { get; } = string.Empty;
   public string Country { get; } = string.Empty;
   public string State { get; } = string.Empty;
   public string ZipCode { get; } = string.Empty;
+
+  public static Address Of(string firstName, string lastName, string emailAddress, string addressLine, string country,
+    string state, string zipCode)
+  {
+    ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+    ArgumentException.ThrowIfNullOrWhiteSpace(addressLine);
+    return new Address(firstName, lastName, emailAddress, addressLine, country, state, zipCode);
+  }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -2,9 +2,31 @@ namespace Ordering.Domain.ValueObjects;
 
 public record Payment
 {
-  public string? CardName { get; } = null!;
+  protected Payment()
+  {
+  }
+
+  private Payment(string cardName, string cardNumber, string expiration, string cvv, string paymentMethod)
+  {
+    CardName = cardName;
+    CardNumber = cardNumber;
+    Expiration = expiration;
+    CVV = cvv;
+    PaymentMethod = paymentMethod;
+  }
+
+  public string? CardName { get; }
   public string CardNumber { get; } = null!;
   public string Expiration { get; } = null!;
   public string CVV { get; } = null!;
   public string PaymentMethod { get; } = null!;
+
+  public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, string paymentMethod)
+  {
+    ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+    ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+    ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+    ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+    return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+  }
 }
